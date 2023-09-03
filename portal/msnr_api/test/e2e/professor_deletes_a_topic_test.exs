2b3c044a52aaebdb865bd381513bb72f78bd3260
Andjela <44590852+endzikri@users.noreply.github.com>
2023-09-04 01:24:56 +0200

    added comments for deleting topic

--- portal/msnr_api/test/e2e/professor_deletes_a_topic_test.exs
+++ portal/msnr_api/test/e2e/professor_deletes_a_topic_test.exs
@@ -10,6 +10,7 @@ defmodule ProfessorCreatesATopicTest do
 	#Za ovaj test neophodno je da postoji barem jedna tema u bazi
     maximize_window(current_window_handle())
 
+    # logovanje profesora
     navigate_to("http://localhost:8080")
 	:timer.sleep(1000) 
 	assert {:ok, _element} = search_element(:link_text, "Prijavi se")
@@ -19,7 +20,8 @@ defmodule ProfessorCreatesATopicTest do
 	fill_field({:id, "Nri-Ui-TextInput-Password"}, "test")
 	find_element(:class, "_4d72d302") |> click()
 	:timer.sleep(2000)
-	
+
+	# biramo tab sa temama
 	maximize_window(current_window_handle())
 	header = find_element(:class, "_c7f4942c")
     tabs = find_within_element(header, :class, "_84f7a906")
@@ -29,7 +31,8 @@ defmodule ProfessorCreatesATopicTest do
 	topics |> click()
 	:timer.sleep(3000)
 	assert current_path() == "/professor/topics"
-	
+
+	# brisemo prvu temu
 	body = find_element(:class, "_6d6da8c9")
 	topicsBody = find_within_element(body, :class, "_fd567d89")
 	topicsList = find_all_within_element(topicsBody, :class, "_ba485483")
